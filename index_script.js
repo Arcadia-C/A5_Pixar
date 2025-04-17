be307fd097e75f8937ea5286e01d3ecbdf8e990d
goutzou <ngoutzoulias@gmail.com>
2025-04-17 16:52:24 -0400

    d3 import fix

--- index_script.js
+++ index_script.js
@@ -1,4 +1,4 @@
-import * as d3 from "d3";
+// import * as d3 from "d3";
 
 // Sample data
 const data = [4, 8, 15, 16, 23, 42];
@@ -9,40 +9,44 @@ const height = 400;
 const margin = { top: 20, right: 20, bottom: 30, left: 40 };
 
 // Create the SVG container
-const svg = d3.select("#visualization")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+const svg = d3
+  .select("#visualization")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
 
 // Create scales
-const x = d3.scaleBand()
-    .range([margin.left, width - margin.right])
-    .padding(0.1);
+const x = d3
+  .scaleBand()
+  .range([margin.left, width - margin.right])
+  .padding(0.1);
 
-const y = d3.scaleLinear()
-    .range([height - margin.bottom, margin.top]);
+const y = d3.scaleLinear().range([height - margin.bottom, margin.top]);
 
 // Set the domains
 x.domain(d3.range(data.length));
 y.domain([0, d3.max(data)]);
 
 // Add the bars
-svg.selectAll("rect")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr("x", (d, i) => x(i))
-    .attr("y", d => y(d))
-    .attr("width", x.bandwidth())
-    .attr("height", d => height - margin.bottom - y(d))
-    .attr("fill", "steelblue");
+svg
+  .selectAll("rect")
+  .data(data)
+  .enter()
+  .append("rect")
+  .attr("x", (d, i) => x(i))
+  .attr("y", (d) => y(d))
+  .attr("width", x.bandwidth())
+  .attr("height", (d) => height - margin.bottom - y(d))
+  .attr("fill", "steelblue");
 
 // Add x-axis
-svg.append("g")
-    .attr("transform", `translate(0,${height - margin.bottom})`)
-    .call(d3.axisBottom(x));
+svg
+  .append("g")
+  .attr("transform", `translate(0,${height - margin.bottom})`)
+  .call(d3.axisBottom(x));
 
 // Add y-axis
-svg.append("g")
-    .attr("transform", `translate(${margin.left},0)`)
-    .call(d3.axisLeft(y));
+svg
+  .append("g")
+  .attr("transform", `translate(${margin.left},0)`)
+  .call(d3.axisLeft(y));
